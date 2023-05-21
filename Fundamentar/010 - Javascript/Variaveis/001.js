dd231a640a17bb9e13cf47986072ee282beb90ba
gustavogarciac <132074066+gustavogarciac@users.noreply.github.com>
2023-05-21 00:26:40 -0300

    Continuidade estudo variaveis/ Estudo sobre escopos

--- Fundamentar/010 - Javascript/Variaveis/001.js
+++ Fundamentar/010 - Javascript/Variaveis/001.js
@@ -7,7 +7,28 @@ let
 const -> permanece com o valor inalterado.
 
 */
-var clima = "Quente";
-console.log(clima)
-let nome = "Gustavo";
-console.log(nome)
+//var clima = "Quente";
+//console.log(clima)
+//let nome = "Gustavo";
+//console.log(nome)
+/* SCOPE
+Determina a visibilidade de alguma variável no JS 
+VAR -> possui escopo global e, também local. Capaz de Hoisting.
+LET -> Apenas existem dentro do escopo criado. 
+*/
+
+console.log('Existe x antes do bloco?', x)
+
+{
+    //Aqui dentro é um bloco e posso colocar qualquer código.
+    var x = 0
+    console.log(x)
+}
+
+console.log('Existe y antes do bloco?', `Não.`)
+let y = 1
+{
+    let y = 0
+    console.log(y)
+}
+console.log(`Existe y depois do bloco?`, y) 
