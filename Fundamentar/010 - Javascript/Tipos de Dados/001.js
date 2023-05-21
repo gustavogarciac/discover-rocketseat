7c9b5dbeead318ae0f87ca59906fbda789796577
gustavogarciac <132074066+gustavogarciac@users.noreply.github.com>
2023-05-21 00:03:43 -0300

    Estudos Javascript, Tipos de Dados + Variaveis

---
+++ Fundamentar/010 - Javascript/Tipos de Dados/001.js
@@ -0,0 +1,58 @@
+// TIPOS DE DADOS
+
+/* STRING
+É uma cadeia de caracteres.
+
+Pode ser utilizados através de:
+
+""
+''
+`` = Permite múltiplas linhas/expressões
+
+*/
+
+
+//let example_string = 'Gustavo'
+//console.log(`A variável foi declada com a string -> ${example_string}`)
+
+/* NUMBER
+33 -> inteiros
+12.5 -> reais
+NaN -> Not a Number
+Infinity -> infinito
+*/
+
+/* BOOLEAN
+Possui apenas 2 valores:
+verdadeiro
+falso
+*/
+
+/* Undefined e Null 
+Undefined = é algo indefinido.
+null = nulo, objeto sem nada dentro.
+*/
+
+/* Object
+É um dado estrutural, possui propriedades, atributos, funcionalidades e métodos.
+{ propriedade = "valor" }
+
+console.log({
+    name: "Gustavo",
+    age: "18",
+    andar: function() {
+        console.log('Andar.')
+    }
+})*/
+
+/* ARRAY = VETORES
+
+É uma lista, um agrupamento de dados.
+var nomes = []
+
+
+
+console.log([
+    "Gustavo", 
+    18
+])*/
---
+++ Fundamentar/010 - Javascript/Variaveis/001.js
@@ -0,0 +1,13 @@
+/* VARIÁVEIS
+Utilizadas para o armazenamento de dados.
+Pode-se utilizar 3 palavras reservadas:
+
+var
+let
+const -> permanece com o valor inalterado.
+
+*/
+var clima = "Quente";
+console.log(clima)
+let nome = "Gustavo";
+console.log(nome)
